afde55ad09ed49dff8f3153cc52e852e86b0ab27
javier <javiro76@gmail.com>
2023-04-13 12:02:32 -0500

    primer commit

---
+++ src/breakingbad/brakingbad-app.js
@@ -0,0 +1,14 @@
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+
+export const BreakingbadApp = ( element ) => { 
+   document.querySelector("#app-tittle").innerHTML = `Breakingbad App`;
+   element.innerHTML = `Loading...`;
+
+}
+
+
