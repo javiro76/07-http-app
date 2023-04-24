ce2c96e5fa8d0516fac07299e15f8408e40bdcbd
javier <javiro76@gmail.com>
2023-04-24 15:14:41 -0500

    se crea addButton

---
+++ src/users/presentation/render-add-button/render-add-button.js
@@ -0,0 +1,20 @@
+import './render-add-button.css';
+
+
+
+
+
+export const renderAddButton = (element) => {
+
+    const fabButton = document.createElement('button');
+    fabButton.innerText = '+';
+    fabButton.classList.add('fab-button');
+
+    element.append (fabButton);
+
+    fabButton.addEventListener('click', () => {
+        throw Error ('Not implemented yet');
+})
+
+
+};
